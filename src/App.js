65b3c52b4cd1b7212158a4ac2c10f486920b9720
Maria Shvyrkova <shvyrkovamaria@gmail.com>
2021-09-09 17:28:15 +0300

    Add API

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { fetchExchage } from './services/exchangeApi';
+fetchExchage('01.12.2014');
 function App() {
   return <div></div>;
 }
---
+++ src/services/exchangeApi.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://api.privatbank.ua/p24api';
+
+export async function fetchExchage(date) {
+  const data = await axios.get(`/exchange_rates?json&date=${date}`);
+  console.log(data);
+  return data;
+}
